70c4087b4fba9007d12f6c2c96a036f0c9d60efa
hiếu hàn <hantrunghieu@hotmail.com>
2018-07-30 13:36:40 +0700

    ...

--- HanSoft.CMSLib/DbHelper.cs
+++ HanSoft.CMSLib/DbHelper.cs
@@ -1,12 +1,131 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HanSoft.HelperLib;
 
 namespace HanSoft.CMSLib
 {
     public class DbHelper
     {
+        #region Private attribute
+
+        private static string _defaultProviderName = ConfigHelper.GetConfigString("dbProviderName");
+        private static string _defaultConnectionString = ConfigHelper.GetConfigString("dbConnection");
+        private static DbHelper _instance = new DbHelper(_defaultConnectionString, _defaultProviderName);
+
+        public static DbHelper Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        private string _providerName;
+        private string _connectionString;
+
+        public string ProviderName
+        {
+            get { return _providerName; }
+            set { _providerName = value; }
+        }
+
+        public string ConnectionString
+        {
+            get { return _connectionString; }
+            set { _connectionString = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+        public DbHelper()
+        {
+
+        }
+
+        public DbHelper(string connectionString, string providerName)
+        {
+            this._connectionString = connectionString;
+            this._providerName = providerName;
+        }
+
+        #endregion
+
+        #region Create object
+        public DbConnection CreateConnection()
+        {
+            DbProviderFactory dbfactory = DbProviderFactories.GetFactory(_providerName);
+            DbConnection dbconn = dbfactory.CreateConnection();
+            dbconn.ConnectionString = _connectionString;
+            return dbconn;
+        }
+
+        public DbCommand CreateCommand()
+        {
+            DbProviderFactory dbfactory = DbProviderFactories.GetFactory(_providerName);
+            return dbfactory.CreateCommand();
+        }
+
+        public DbCommand CreateSqlsCommand(string sqls)
+        {
+            DbProviderFactory dbfactory = DbProviderFactories.GetFactory(_providerName);
+            DbCommand cmd = dbfactory.CreateCommand();
+            cmd.CommandText = sqls;
+            cmd.CommandType = CommandType.Text;
+            return cmd;
+        }
+
+        public DbCommand CreateProcCommand(string proc)
+        {
+            DbProviderFactory dbfactory = DbProviderFactories.GetFactory(_providerName);
+            DbCommand cmd = dbfactory.CreateCommand();
+            cmd.CommandText = proc;
+            cmd.CommandType = CommandType.StoredProcedure;
+            return cmd;
+        }
+
+        public DbParameter CreateParameter()
+        {
+            DbProviderFactory dbfactory = DbProviderFactories.GetFactory(_providerName);
+            return dbfactory.CreateParameter();
+        }
+
+        public DbParameter CreateParameter(string name, object value)
+        {
+            DbProviderFactory dbfactory = DbProviderFactories.GetFactory(_providerName);
+            DbParameter p = dbfactory.CreateParameter();
+            p.ParameterName = name;
+            p.Value = value;
+            return p;
+        }
+
+        public DbParameter CreateParameter(string name, object value, DbType dbType)
+        {
+            DbProviderFactory dbfactory = DbProviderFactories.GetFactory(_providerName);
+            DbParameter p = dbfactory.CreateParameter();
+            p.ParameterName = name;
+            p.DbType = dbType;
+            p.Value = value;
+            return p;
+        }
+
+        public DbParameter CreateParameter(string name, object value, DbType dbType, int size)
+        {
+            DbProviderFactory dbfactory = DbProviderFactories.GetFactory(_providerName);
+            DbParameter p = dbfactory.CreateParameter();
+            p.ParameterName = name;
+            p.DbType = dbType;
+            p.Value = value;
+            p.Size = size;
+            return p;
+        }
+
+        #endregion
+
     }
 }
---
+++ HanSoft.HelperLib/ConfigHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanSoft.HelperLib
+{
+    public class ConfigHelper
+    {
+        #region Constructor
+        public ConfigHelper()
+        {
+        }
+        #endregion
+
+        #region Methods
+        public static string GetConfigString(string key)
+        {
+            string cacheKey = "AppSettings-" + key;
+            object objModel = CacheHelper.GetCache(cacheKey);
+            if (objModel == null)
+            {
+                try
+                {
+                    objModel = ConfigurationManager.AppSettings[key];
+                    if (objModel != null)
+                    {
+                        CacheHelper.SetCache(cacheKey, objModel, DateTime.Now.AddMinutes(180), TimeSpan.Zero);
+                    }
+                }
+                catch
+                { }
+            }
+            return objModel.ToString();
+        }
+        #endregion
+    }
+}
